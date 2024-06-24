3b4ca0282f194be9a92a31cafce41fc683c3f52f
unknown <tatjanajovicic19@gmail.com>
2024-06-24 22:37:03 +0200

    add Home page and active class for nav from feat-Home

--- src/features/Navigation/Navigation.jsx
+++ src/features/Navigation/Navigation.jsx
@@ -1,18 +1,42 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import "./Navigation.css";
 import PageLogo from "../../components/PageLogo/PageLogo";
 
 const Navigation = () => {
+  const location = useLocation();
+
   return (
     <div className="navigation">
       <PageLogo />
       <hr />
       <nav className="nav">
         <div className="nav_links">
-          <Link to={{ pathname: "/" }}>Home</Link>
-          <Link to={{ pathname: "/book_movie" }}>Book Movie</Link>
-          <Link to={{ pathname: "/how_to_book" }}>How To Book</Link>
-          <Link to={{ pathname: "/contact" }}>Contact</Link>
+          <Link
+            to="/"
+            className={location.pathname === "/" ? "active-link" : ""}
+          >
+            Home
+          </Link>
+          <Link
+            to="/book_movie"
+            className={location.pathname === "/book_movie" ? "active-link" : ""}
+          >
+            Book Movie
+          </Link>
+          <Link
+            to="/how_to_book"
+            className={
+              location.pathname === "/how_to_book" ? "active-link" : ""
+            }
+          >
+            How To Book
+          </Link>
+          <Link
+            to="/contact"
+            className={location.pathname === "/contact" ? "active-link" : ""}
+          >
+            Contact
+          </Link>
         </div>
       </nav>
     </div>
