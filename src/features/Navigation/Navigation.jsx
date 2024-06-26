1ede39bde0384472882484567c03350bf5d2c901
unknown <tatjanajovicic19@gmail.com>
2024-06-26 17:06:28 +0200

    update navigation from feat-Navigation

--- src/features/Navigation/Navigation.jsx
+++ src/features/Navigation/Navigation.jsx
@@ -1,25 +1,40 @@
 import { Link, useLocation } from "react-router-dom";
 import "./Navigation.css";
 import PageLogo from "../../components/PageLogo/PageLogo";
+import { useState } from "react";
 
 const Navigation = () => {
   const location = useLocation();
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  const closeMenu = () => {
+    setMenuOpen(false);
+  };
 
   return (
     <div className="navigation">
       <PageLogo />
       <hr />
-      <nav className="nav">
+      <button className="menu-icon" onClick={toggleMenu}>
+        &#9776;
+      </button>
+      <nav className={`nav ${menuOpen ? "open" : ""}`}>
         <div className="nav_links">
           <Link
             to="/"
             className={location.pathname === "/" ? "active-link" : ""}
+            onClick={closeMenu}
           >
             Home
           </Link>
           <Link
             to="/book_movie"
             className={location.pathname === "/book_movie" ? "active-link" : ""}
+            onClick={closeMenu}
           >
             Book Movie
           </Link>
@@ -28,12 +43,14 @@ const Navigation = () => {
             className={
               location.pathname === "/how_to_book" ? "active-link" : ""
             }
+            onClick={closeMenu}
           >
             How To Book
           </Link>
           <Link
             to="/contact"
             className={location.pathname === "/contact" ? "active-link" : ""}
+            onClick={closeMenu}
           >
             Contact
           </Link>
