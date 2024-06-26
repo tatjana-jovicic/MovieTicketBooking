cfd352c26e0155aa804014ddfba5a63b15f99417
unknown <tatjanajovicic19@gmail.com>
2024-06-26 13:59:04 +0200

    add background image

--- src/components/PageLogo/PageLogo.jsx
+++ src/components/PageLogo/PageLogo.jsx
@@ -1,12 +1,10 @@
 import "./PageLogo.css";
+import PageLogoImg from "../../assets/page-logo.png";
 
 const PageLogo = () => {
   return (
     <div className="page_logo">
-      <img
-        src="https://png.pngtree.com/png-vector/20230112/ourmid/pngtree-ticket-icon-vector-symbol-illustration-on-white-background-png-image_6559386.png"
-        alt="page logo"
-      />
+      <img src={PageLogoImg} alt="page logo" />
     </div>
   );
 };
