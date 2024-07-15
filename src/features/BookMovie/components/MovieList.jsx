a4a6a784172ff8e474f6672e637ec0d56bdbda03
unknown <tatjanajovicic19@gmail.com>
2024-07-15 23:58:17 +0200

    update book movie page and add logic for search

--- src/features/BookMovie/components/MovieList.jsx
+++ src/features/BookMovie/components/MovieList.jsx
@@ -1,16 +1,58 @@
+// import "../BookMovie.css";
+// import { movies } from "../../../data/moviesdata";
+// import useSelectMovieStore from "../../../stores/select_movie/selectMovie.store";
+// import MovieRating from "./MovieRating";
+
+// const MovieList = ({ filter, filterValue, onMovieClick }) => {
+//   const setSelectedMovie = useSelectMovieStore(
+//     (state) => state.setSelectedMovie
+//   );
+
+//   const filteredMovies = movies.filter(
+//     (movie) => movie[filter] === filterValue
+//   );
+
+//   const handleMovieClick = (movie) => {
+//     setSelectedMovie(movie);
+//     onMovieClick(movie);
+//   };
+
+//   return (
+//     <div className="movies">
+//       {filteredMovies.map((movie) => (
+//         <div
+//           className="movie_list"
+//           key={movie.id}
+//           onClick={() => handleMovieClick(movie)}
+//           title="Click for book movie"
+//         >
+//           <img src={movie.image} alt={movie.name} />
+//           <MovieRating rating={movie.rating} />
+//           <div className="movie_name">
+//             <h3>{movie.name}</h3>
+//           </div>
+//         </div>
+//       ))}
+//     </div>
+//   );
+// };
+
+// export default MovieList;
+
+//ovo gore je staro stanje bez logike search
+
 import "../BookMovie.css";
-import { movies } from "../../../data/moviesdata";
+import { movies as allMovies } from "../../../data/moviesdata";
 import useSelectMovieStore from "../../../stores/select_movie/selectMovie.store";
 import MovieRating from "./MovieRating";
 
-const MovieList = ({ filter, filterValue, onMovieClick }) => {
+const MovieList = ({ filter, filterValue, onMovieClick, movies }) => {
   const setSelectedMovie = useSelectMovieStore(
     (state) => state.setSelectedMovie
   );
 
-  const filteredMovies = movies.filter(
-    (movie) => movie[filter] === filterValue
-  );
+  const filteredMovies =
+    movies || allMovies.filter((movie) => movie[filter] === filterValue);
 
   const handleMovieClick = (movie) => {
     setSelectedMovie(movie);
